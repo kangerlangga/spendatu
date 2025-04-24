07075a89983a1a505a906ece7024018b14a1137d
kangerlangga <erlanggalesmanaputra@gmail.com>
2025-04-24 16:48:04 +0700

    Update 20250424 1648

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -185,7 +185,7 @@
 			</div>
 			<h2>We are sorry, Page not found!</h2>
 			<p>The page you are looking for might have been removed had its name changed or is temporarily unavailable.</p>
-			<a href="#">Back To Homepage</a>
+			<a href="{{ route('beranda.publik') }}">Back To Homepage</a>
 		</div>
 	</div>
     <script>
